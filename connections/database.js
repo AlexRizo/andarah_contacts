52082585b493cd54ab3c0e82093c17c951c6714d
Alex <david_alejandrorizo@outlook.com>
2022-12-23 10:50:26 -0600

    p2

--- connections/database.js
+++ connections/database.js
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize('andarah', 'root', 'qDMoku9jSTuGoaJ9L8L8', {
     host: 'containers-us-west-174.railway.app',
     dialect: 'mysql',
+    port: '7112'
 });
 
 export default sequelize;
