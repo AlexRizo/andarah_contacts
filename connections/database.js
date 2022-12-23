9b233980b83a0019e17f032f085ffab569f16a16
Alex <david_alejandrorizo@outlook.com>
2022-12-23 10:53:48 -0600

    p3

--- connections/database.js
+++ connections/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('andarah', 'root', 'qDMoku9jSTuGoaJ9L8L8', {
+const sequelize = new Sequelize('useful-war', 'root', 'qDMoku9jSTuGoaJ9L8L8', {
     host: 'containers-us-west-174.railway.app',
     dialect: 'mysql',
     port: '7112'
