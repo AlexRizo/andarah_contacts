5e144592b8ff8055a80e6aae89bcfe3db4761751
Alex <david_alejandrorizo@outlook.com>
2023-01-25 11:45:44 -0600

    db conn fix

--- connections/database.js
+++ connections/database.js
@@ -1,11 +1,5 @@
 import { Sequelize } from "sequelize";
-// const sequelize = new Sequelize( 'andarah', 'andarah-leadcenter', 'FyjAGPxv2Ru2206fKdhn', {
-//     host: '127.0.0.1',
-//     dialect: 'mysql',
-//     port: 3306,
-// });
-
-const sequelize = new Sequelize( 'andarah', 'root', '78204991+?-', {
+const sequelize = new Sequelize( 'andarah', 'andarah-leadcenter', 'FyjAGPxv2Ru2206fKdhn', {
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
